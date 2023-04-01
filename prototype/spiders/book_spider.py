a2fe076ef684174381139db3d6c79edb80e52296
Zachary Perales <aardzarks@gmail.com>
2023-04-01 16:08:31 -0400

    Add documentation.

--- prototype/spiders/book_spider.py
+++ prototype/spiders/book_spider.py
@@ -14,30 +14,40 @@ class BookSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.page_parse)
 
     def page_parse(self, response: scrapy.http.Response) -> None:
-        print(f'Page url: {response.url}')
+        # Log the method name and response url to assist in visualizing Scrapy's logic flow
+        self.logger.debug(f'Inside page_parse for response: {response.url}')
 
+        # Try to make requests if response is not equal to None
         try:
+            # Make recursive requests to page_parse and set the parse_method
+            # and pass the parse method as metadata
+            yield scrapy.Request(url=response.meta.get('next_page_url'),
+                                 callback=self.page_parse,
+                                 meta={'parse_method': 'page_parse'}) # Add this metadata to start request as well
+
+            # Make an initial request to book_parse and pass the book urls as metadata
             yield scrapy.Request(url=response.meta.get('book urls')[0],
                                  callback=self.book_parse,
                                  meta={'parse_method': 'book_parse',
-                                       'book urls': response.meta.get('book urls')})
+                                       'book urls': response.meta.get('book urls')}) # Check if this metadata persists
 
-            yield scrapy.Request(url=response.meta.get('next_page_url'),
-                                 callback=self.page_parse,
-                                 meta={'parse_method': 'page_parse'})
-
+        # If we are out of requests have Scrapy close the spider
         except TypeError:
             print("\x1b[31mOUT OF BOOKS\x1b[0m")
-            pass
+            pass # check if we should change this to return
 
     def closed(self, reason: str) -> None:
         self.logger.critical('Spider closed: %s', reason)
 
     def book_parse(self, response: scrapy.http.Response) -> None:
-        print(f'Book url: {response.url}')
+        # Log the method name and response url to assist in visualizing Scrapy's logic flow
+        self.log(f'Inside book_parse for response: {response.url}')
+
         book_urls: List[str] = response.meta.get('book urls')
 
-        if book_urls.index(response.url) + 1 < len(book_urls):
+        # Check if the current url is within range of the list length of book urls
+        if book_urls.index(response.url) + 1 < len(book_urls): # Check if this can instead be a try/except block
+            # Make recursive requests to page_parse and set the parse_method
             yield scrapy.Request(
                 url=book_urls[book_urls.index(response.url) + 1],
                 callback=self.book_parse,
