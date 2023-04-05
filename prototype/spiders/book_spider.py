b3560afdda826000f298365388b64d8d37d86a6a
Zachary Perales <aardzarks@gmail.com>
2023-04-05 08:52:29 -0400

    Revert the unidecode solution and remove unused imports.
    We were able to modify the client encoding from WIN1252 to UTF8 in the DBMS via the psql CLI. This resolved the second defect but the description column needed to remain at a size of 10000 characters due to some descriptions exceeding 5000 characters.
    
    Closes #8

--- prototype/spiders/book_spider.py
+++ prototype/spiders/book_spider.py
@@ -1,10 +1,8 @@
-import scrapy
 from typing import List, Dict, Generator
 from scrapy import Request, Spider
 from scrapy.http import Response
 from prototype.items import BookItem
-import chardet
-from unidecode import unidecode
+
 
 class BookSpider(Spider):
     name: str = "books"
@@ -36,8 +34,7 @@ class BookSpider(Spider):
         book_item = BookItem()
 
         book_item['title'] = response.css('.product_page h1::text').get()
-        # Use unidecode to convert any non-ASCII characters to their closest ASCII equivalent
-        book_item['description'] = unidecode(response.css('#product_description + p::text').get())
+        book_item['description'] = response.css('#product_description + p::text').get()
         book_item['upc'] = response.css('th:contains("UPC") + td::text').get()
         book_item['price'] = response.css('th:contains("Price (excl. tax)") + td::text').get().split("£")[1]
         book_item['tax'] = response.css('th:contains("Tax") + td::text').get().split("£")[1]
