2b28468f8dc96205b6f804c596f42fcd1d315467
Zachary Perales <aardzarks@gmail.com>
2023-04-07 16:51:26 -0400

    Refactor directory structure.

--- prototype/spiders/__init__.py
+++
@@ -1,8 +0,0 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-__title__ = 'Spiders'
-__version__ = '1.0.0'
-
-from .book_spider import BookSpider
