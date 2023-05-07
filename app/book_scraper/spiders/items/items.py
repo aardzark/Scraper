0eac494b139cdb04c03f7546e9009f415a062fcb
Zachary Perales <zacharyromeperales@gmail.com>
2023-05-07 06:42:27 -0400

    Add BookItem Fields for downloading images
    References #13

--- app/book_scraper/spiders/items/items.py
+++ app/book_scraper/spiders/items/items.py
@@ -4,9 +4,12 @@ from scrapy import Field, Item
 class BookItem(Item):
     title: Field = Field()
     description: Field = Field()
+    image_urls: Field = Field()
     upc: Field = Field()
     price: Field = Field()
     tax: Field = Field()
     stock: Field = Field()
     number_of_reviews: Field = Field()
+
+    images: Field = Field()
     pass
